40ba1a0387c4f9ca7356b304e132ae88192b0e51
PAITnay <vadym.platanyi@nure.ua>
2023-03-14 23:33:16 +0200

    Setup unit test class and submodule versions update

--- dotnet-unit/UnitTest1.cs
+++ dotnet-unit/UnitTest1.cs
@@ -1,10 +1,20 @@
+using Lab2;
 namespace dotnet_unit;
 
-public class UnitTest1
+
+public class UnitTestDatabase
 {
+    Cinema cinema;
+    public UnitTestDatabase()
+    {
+        cinema = new Cinema();
+    }
+
     [Fact]
     public void Test1()
     {
+        cinema.RemoveMovie("mvi1");
+        /// cinema.ticketsContext.Movies
         Assert.True(true);
     }
 }
